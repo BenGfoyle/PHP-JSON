d7d82f8932dd515ab9552455bd63d54b3611506f
Ben Guilfoyle <40490843+BenGfoyle@users.noreply.github.com>
2019-05-02 17:51:18 +0100

    trying to get "editThis" to work

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
 <?php
-
 function getJSON(){
   // read json file
   $data = file_get_contents('readingList.json');
-
   // decode json to associative array
   $json_arr = json_decode($data, true);
   return $json_arr;
 }
-
 function checkDupes($name){
   //check for duplicates by checking for error
   json_decode($name);
   return (json_last_error() == JSON_ERROR_NONE);
 }
-
 function userID(){
     //generate unique user ID based on username
       $name = $_POST['name'];
@@ -22,67 +18,105 @@ function userID(){
       for($i = 0; $i < strlen($name); $i++){
         $hash = $hash + (ord($name[$i])**$i);
       }
-
       //keep string small by changing base
       $hash = base64_encode($hash);
-
       return $hash;
 }
-
 $message = '';
 $error = '';
-
-// Edit Data
-if(isset($_POST["add"]))
-{
-     if(empty($_POST["name"]))//check for blanks
-     {
-          $error = "<label class='text-danger'>Enter Name</label>";
-     }
-     else if(empty($_POST["url"])) //check for blanks
-     {
-          $error = "<label class='text-danger'>Enter URL</label>";
-     }
-     else //add the data
-     {
-          if(file_exists('readingList.json')) //&& checkDupes($_POST['name']) == false)
-          {
-               $json_arr = getJSON();
-               $currentDate = time();
-               $newID = userID();
-
-               $extra = array(
-                    'name'               =>     $_POST['name'],
-                    'url'          =>     $_POST["url"],
-                    'date'     =>     $currentDate,
-                    'id'      =>     $newID
-               );
-               $array_data[] = $extra;
-               $final_data = json_encode($array_data);
-               if(file_put_contents('readingList.json', $final_data))
-               {
-                    $message = "<label class='text-success'>File Appended Success fully</p>";
-               }
-          }
-          else
-          {
-               $message = "Name already taken, please try again";
-               echo "<script type='text/javascript'>alert('$message');</script>";
-          }
-     }
+// Add Data
+function addThis(){
+  if(empty($_POST["name"]))//check for blanks
+  {
+       $error = "<label class='text-danger'>Enter Name</label>";
+  }
+  else if(empty($_POST["url"])) //check for blanks
+  {
+       $error = "<label class='text-danger'>Enter URL</label>";
+  }
+  else //add the data
+  {
+       if(file_exists('readingList.json')) //&& checkDupes($_POST['name']) == false)
+       {
+            $json_arr = getJSON();
+            $currentDate = time();
+            $newID = userID();
+            $extra = array(
+                 'name'               =>     $_POST['name'],
+                 'url'          =>     $_POST["url"],
+                 'date'     =>     $currentDate,
+                 'id'      =>     $newID
+            );
+            $json_arr[] = $extra;
+            $final_data = json_encode($json_arr);
+            if(file_put_contents('readingList.json', $final_data))
+            {
+                 $message = "<label class='text-success'>File Appended Success fully</p>";
+            }
+       }
+       else
+       {
+            $message = "Name already take/File not found";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+       }
+  }
 }
 
 //Delte Data
-if(isset($_POST["delete"])){
-    $json_arr = getJSON();
-
-    // get array index to delete
-    $arr_index = array();
-    foreach ($json_arr as $key => $value)
+function deleteThis(){
+  $json_arr = getJSON();
+  // get array index to delete
+  $arr_index = array();
+  foreach ($json_arr as $key => $value)
+  {
+    if ($value['id'] == $_POST['id'] || $value['name'] == $_POST['name'])
     {
-      if ($value['id'] == $_POST['delID'] || $value['name'] == $_POST['delName'])
-      {
-          $arr_index[] = $key;
+        $arr_index[] = $key;
+    }
+  }
+  // delete data
+  foreach ($arr_index as $i)
+  {
+    unset($json_arr[$i]);
+  }
+  // rebase array
+  $json_arr = array_values($json_arr);
+  // encode array to json and save to file
+  file_put_contents('readingList.json', json_encode($json_arr));
+}
+
+//Get Data
+function getThis(){
+  $json_arr = getJSON();
+  // get array index to delete
+  $arr_index = array();
+
+
+  // rebase array
+  $json_arr = array_values($json_arr);
+  // encode array to json and save to file
+  file_put_contents('readingList.json', json_encode($json_arr));
+}
+
+//Edit Data
+function editThis(){
+  $json_arr = getJSON();
+  // get aBenrray index to delete
+  $arr_index = array();
+
+
+  for($i = 0; $i < sizeof($json_arr); $i++){
+    if ($json_arr.id.[$i] == $_POST["name"]){
+      $arr_index[] = $key;
+      $newID = userID();
+      $extra = array(
+           'name'               =>     $_POST['name'],
+           'url'          =>     $_POST["url"],
+           'date'     =>     $json_arr[$i]->date,
+           'id'      =>     $newID
+      );
+      $json_arr[] = $extra;
+      deleteThis();
       }
     }
 
@@ -93,40 +127,25 @@ if(isset($_POST["delete"])){
     }
 
     // rebase array
-  $json_arr = array_values($json_arr);
-
-  // encode array to json and save to file
-  file_put_contents('readingList.json', json_encode($json_arr));
-}
-
-//Edit Data
+    $json_arr = array_values($json_arr);
+    // encode array to json and save to file
+    file_put_contents('readingList.json', json_encode($json_arr));
+  }
 if(isset($_POST["edit"])){
-
-  $json_arr = getJSON();
-
-  // get array index to delete
-  $arr_index = array();
-  foreach ($json_arr as $key => $value)
-  {
-    if ($value['id'] == $_POST['delID'] || $value['name'] == $_POST['delName'])
-    {
-        $arr_index[] = $key;
-    }
-  }
-
-  // delete data
-  foreach ($arr_index as $i)
-  {
-    unset($json_arr[$i]);
-  }
-
-  // rebase array
-$json_arr = array_values($json_arr);
-
-// encode array to json and save to file
-file_put_contents('readingList.json', json_encode($json_arr));
+  editThis();
 }
 
+if(isset($_POST["delete"])){
+  deleteThis();
+}
+
+if(isset($_POST["add"])){
+  addThis();
+}
+
+if(isset($_POST["get"])){
+  getThis();
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -136,6 +155,11 @@ file_put_contents('readingList.json', json_encode($json_arr));
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
      </head>
+     <style>
+      table, th, td {
+      border: 1px solid black;
+      }
+    </style>
      <body>
 
        <table align="center">
@@ -144,7 +168,6 @@ file_put_contents('readingList.json', json_encode($json_arr));
                <th>URL</th>
                <th>Date</th>
                <th>ID</th>
-               <th></th>
            </tr>
            <tbody>
                <?php
@@ -161,6 +184,33 @@ file_put_contents('readingList.json', json_encode($json_arr));
            </tbody>
        </table>
 
+<!--Get Data-->
+          <br />
+          <div class="container" style="width:500px;">
+               <h3 align="">Get Data from JSON File</h3><br />
+               <form method="post">
+                    <?php
+                    if(isset($error))
+                    {
+                         echo $error;
+                    }
+                    ?>
+                    <br />
+                    <label>Name</label>
+                    <input type="text" name="name" class="form-control" /><br />
+                    <label>ID</label>
+                    <input type="text" name="id" class="form-control" /><br />
+                    <input type="submit" name="get" value="Retrieve" class="btn btn-info" /><br />
+                    <?php
+                    if(isset($message))
+                    {
+                         echo $message;
+                    }
+                    ?>
+               </form>
+          </div>
+          <br />
+
 <!--Append Data-->
           <br />
           <div class="container" style="width:500px;">
@@ -175,7 +225,7 @@ file_put_contents('readingList.json', json_encode($json_arr));
                     <br />
                     <label>Name</label>
                     <input type="text" name="name" class="form-control" /><br />
-                    <label>url</label>
+                    <label>URL</label>
                     <input type="text" name="url" class="form-control" /><br />
                     <input type="submit" name="add" value="Append" class="btn btn-info" /><br />
                     <?php
@@ -187,7 +237,6 @@ file_put_contents('readingList.json', json_encode($json_arr));
                </form>
           </div>
           <br />
-
 
 <!--Delete Data-->
           <br />
@@ -202,9 +251,9 @@ file_put_contents('readingList.json', json_encode($json_arr));
                     ?>
                     <br />
                     <label>Name</label>
-                    <input type="text" name="delName" class="form-control" /><br />
+                    <input type="text" name="name" class="form-control" /><br />
                     <label>ID</label>
-                    <input type="text" name="delID" class="form-control" /><br />
+                    <input type="text" name="id" class="form-control" /><br />
                     <input type="submit" name="delete" value="Delete" class="btn btn-info" /><br />
                     <?php
                     if(isset($message))
@@ -229,12 +278,10 @@ file_put_contents('readingList.json', json_encode($json_arr));
                     }
                     ?>
                     <br />
-                    <label>Old Name</label>
-                    <input type="text" name="OldName" class="form-control" /><br />
+                    <label>User ID</label>
+                    <input type="text" name="id" class="form-control" /><br />
                     <label>New Name</label>
                     <input type="text" name="newName" class="form-control" /><br />
-                    <label>Old URL</label>
-                    <input type="text" name="oldURL" class="form-control" /><br />
                     <label>New URL</label>
                     <input type="text" name="newURL" class="form-control" /><br />
                     <input type="submit" name="edit" value="Edit" class="btn btn-info" /><br />
